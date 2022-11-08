a1a63ce7ca64e25ecb5247730252c2d7bfac4e8b
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-08 23:29:15 +0700

    Creating ListViewModel.kt to handle LiveData. Adding UI logic to StudentListFragment.kt. Hotfix: Fixing layout bug in student_list_item.xml.

--- app/src/main/java/com/example/belajarmvvm/view/StudentListFragment.kt
+++ app/src/main/java/com/example/belajarmvvm/view/StudentListFragment.kt
@@ -1,18 +1,50 @@
 package com.example.belajarmvvm.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.ListFragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.belajarmvvm.R
+import com.example.belajarmvvm.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_list.*
+
 class StudentListFragment : ListFragment() {
+    private lateinit var viewModel: ListViewModel
+    private var adapter = StudentListAdapter(arrayListOf())
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = adapter
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.studentsLiveData.observe(viewLifecycleOwner) {
+            adapter.updateStudentList(it)
+        }
+
+        viewModel.studentLoadErrorLiveData.observe(viewLifecycleOwner) {
+            txtError.visibility = if (it) View.VISIBLE else View.GONE
+        }
+
+        viewModel.loadingLiveData.observe(viewLifecycleOwner) {
+            progressLoad.visibility = if (it) View.VISIBLE else View.GONE
+            recView.visibility = if (it) View.GONE else View.VISIBLE
+        }
     }
 }
