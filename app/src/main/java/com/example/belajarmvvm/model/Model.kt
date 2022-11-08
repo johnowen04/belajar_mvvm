9106341b1710d2e221eee5d1a682149993679f4f
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-08 22:57:36 +0700

    Create new model file to contain Student class.

---
+++ app/src/main/java/com/example/belajarmvvm/model/Model.kt
@@ -0,0 +1,9 @@
+package com.example.belajarmvvm.model
+
+data class Student(
+    val id: String?,
+    var name: String?,
+    var dob: String?,
+    var phone: String?,
+    var photoUrl: String?
+)
