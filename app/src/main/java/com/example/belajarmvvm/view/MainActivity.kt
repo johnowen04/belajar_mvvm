c869b2504f7d786841e0e34dfb05174ecc0fb61c
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-08 22:52:12 +0700

    Creating three new packages for structural purposes. Preparing Fragment Layout and RecyclerView Item Layout. Adding dependency for SwipeRefreshLayout.

--- app/src/main/java/com/example/belajarmvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/belajarmvvm/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.belajarmvvm
+package com.example.belajarmvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.belajarmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
